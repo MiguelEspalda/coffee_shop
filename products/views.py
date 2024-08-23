e389ae58e335c54d9143aafd2e0e811af0082d05
MiguelEspalda <martinezcobosm@gmail.com>
2024-08-23 10:34:21 -0500

    Creación Aplicación - Productos

---
+++ products/views.py
@@ -0,0 +1,6 @@
+from django.views import generic
+from .forms import ProductForm
+
+class ProductFormView(generic.FormView):
+    template_name = 'products/add_product.html'
+    form_class = ProductForm
